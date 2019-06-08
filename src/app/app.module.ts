d7a6c095fe9380deba22d82862a82262f7c11965
Vinnie <vinnie.rossi12@gmail.com>
2019-06-08 14:28:26 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateContainerModalComponent } from './create-container-modal/create-container-modal.component';
+import { EditContainerModalComponent } from './edit-container-modal/edit-container-modal.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateContainerModalComponent,
+    EditContainerModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
+  entryComponents: [
+    CreateContainerModalComponent,
+    EditContainerModalComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

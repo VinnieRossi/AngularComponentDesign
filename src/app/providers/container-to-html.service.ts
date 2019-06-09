549a8ab332c2d0caec638b25604c0c0ad5d5c9de
Vinnie <vinnie.rossi12@gmail.com>
2019-06-09 08:18:31 -0400

    Fix spacing issue in container html generator
    If there were no properties or events it was outputting extra spaces. Fix this by only adding space when we have those collections

--- src/app/providers/container-to-html.service.ts
+++ src/app/providers/container-to-html.service.ts
@@ -33,34 +33,9 @@ ${componentAsHtmlString.join('\n\n')}
     const events: Array<string> = component.eventEmitters.map(event => `(${event.name})="on${capitalize(event.name)}($event)"`);
 
     const html: string =
-      `<${appSettings.applicationPrefix}-${kebabCase(component.name)} ${properties.join(' ')} ${events.join(' ')}></${appSettings.applicationPrefix}-${kebabCase(component.name)}>`;
+      `<${appSettings.applicationPrefix}-${kebabCase(component.name)}${properties.length ? ' ' : ''}${properties.join(' ')}${events.length ? ' ' : ''}${events.join(' ')}></${appSettings.applicationPrefix}-${kebabCase(component.name)}>`;
 
     return html;
   }
 
 }
-
-// const component2: ComponentModel = {
-//   componentName: 'GoodbyeWorld',
-//   inputProperties: [
-//     //TODO: Have option to generate input with separate name (eg @Input('account-id') id: string;)
-//     {
-//       propertyName: 'charName',
-//       propertyType: SupportedTypescriptTypes.String
-//     },
-//     {
-//       propertyName: 'barName',
-//       propertyType: SupportedTypescriptTypes.String
-//     }
-//   ],
-//   eventEmitters: [
-//     {
-//       emitterName: 'close',
-//       emitterPropertyType: SupportedTypescriptTypes.Any
-//     },
-//     {
-//       emitterName: 'edit',
-//       emitterPropertyType: SupportedTypescriptTypes.Any
-//     }
-//   ]
-// };
